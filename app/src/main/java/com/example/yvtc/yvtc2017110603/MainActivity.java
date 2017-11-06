dfd2a4055d5322e1d420cfffa295a57ff9a43112
Shaojung <shaojung@gmail.com>
2017-11-06 15:04:53 +0800

    copy sqlite file

--- app/src/main/java/com/example/yvtc/yvtc2017110603/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc2017110603/MainActivity.java
@@ -3,11 +3,36 @@ package com.example.yvtc.yvtc2017110603;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            InputStream is = getResources().openRawResource(R.raw.mydata);
+            OutputStream os = new FileOutputStream(getFilesDir() + File.separator + "mydata.sqlite");
+            int read;
+            while ((read = is.read()) != -1)
+            {
+                os.write(read);
+            }
+            os.close();
+            is.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
