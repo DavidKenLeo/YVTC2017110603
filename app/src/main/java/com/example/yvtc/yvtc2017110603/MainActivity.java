022a3febf117b4728be908096fdb74bd4c40705c
Shaojung <shaojung@gmail.com>
2017-11-08 09:52:14 +0800

    use SQLite

--- app/src/main/java/com/example/yvtc/yvtc2017110603/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc2017110603/MainActivity.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -43,6 +45,21 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, mylist);
         lv.setAdapter(adapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_add)
+        {
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void copyDBFile()
