5dcbe4662e38d7d4c4fd234ccab71fc4b4e603c6
Shaojung <shaojung@gmail.com>
2017-11-08 11:19:39 +0800

    add AddActivity

---
+++ app/src/main/java/com/example/yvtc/yvtc2017110603/Phone.java
@@ -0,0 +1,17 @@
+package com.example.yvtc.yvtc2017110603;
+
+/**
+ * Created by yvtc on 2017/11/8.
+ */
+
+public class Phone {
+    public int id;
+    public String username;
+    public String tel;
+    public Phone(int id, String username, String tel)
+    {
+        this.id = id;
+        this.username = username;
+        this.tel = tel;
+    }
+}
